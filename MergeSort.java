c9f5317315bd8524a65ad92c3d2f8e3e267a6e05
chirath <chirath.02@gmail.com>
2016-08-14 19:24:49 +0530

    Fixed output

--- MergeSort.java
+++ MergeSort.java
@@ -2,35 +2,66 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 class MergeSort {
-    void merge(int Array[], int startPos, int endPos, int pos) {
-        int i=0, j=0, lengthleft = pos - startPos+1, lengthright = endPos - pos;
-        int left[] = new int[lengthleft];
-        int right[] = new int[lengthright];
-        //System.arraycopy(src, srcPos, dest, destPos, length)
-        System.arraycopy(Array, startPos, left, 0, lengthleft);
-        System.arraycopy(Array, pos, right, 0, lengthright);
-        int k=startPos;
-        while(i < left.length && j < right.length) {
-            if(left[i] < right[j])
-                Array[k++] = left[i++];
+    void merge(int arr[], int l, int m, int r) {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 =  r - m;
+
+        /* create temp arrays */
+        int L[] = new int[n1], R[] = new int[n2];
+
+        /* Copy data to temp arrays L[] and R[] */
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1+ j];
+
+        /* Merge the temp arrays back into arr[l..r]*/
+        i = 0; // Initial index of first subarray
+        j = 0; // Initial index of second subarray
+        k = l; // Initial index of merged subarray
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
             else
-                Array[k++] = right[j++];
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
         }
-        while(i < left.length)
-            Array[k++] = left[i++];
-        while(j < right.length)
-            Array[k++] = right[j++];
+
+        /* Copy the remaining elements of L[], if there
+           are any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy the remaining elements of R[], if there
+           are any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
     void mergeSort(int Array[], int startPos, int endPos) {
-        if(Math.abs(endPos - startPos) < 2)
+        if(endPos - startPos < 1)
             return;
-        int pos = (endPos + startPos)/2;
+        int pos = (startPos + (endPos-1))/2;
         mergeSort(Array, startPos, pos);
         mergeSort(Array, pos + 1, endPos);
-        merge(Array, startPos, endPos, pos);
-        System.out.println(Arrays.toString(Array));
-    }
+        merge(Array, startPos, pos, endPos);
+        }
     public static void main(String[] args) {
         int Array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         MergeSort M = new MergeSort();
