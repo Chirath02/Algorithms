3618e905d31f2e9e3a99bc131fc490ee8cc42907
Chirath02 <chirath.02@gmail.com>
2016-11-18 11:59:12 +0530

    Completed week 2

---
+++ DesignandAnalysisofAlgorithms,Part1/week2/quick_sort_median.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int c = 0;
+
+
+int partition(vector<int> &v, int l, int r) {
+    int p = v[l], t;
+    int i = l;
+    for(int j=i+1; j <= r; ++j) {
+        if(v[j] < p) {
+            i++;
+            t = v[i];
+            v[i] = v[j];
+            v[j] = t;
+        }
+    }
+    t = v[l];
+    v[l] = v[i];
+    v[i] = t;
+
+    return i;
+}
+
+void quickSort(vector<int> &v, int l, int r) {
+    if(l>r)
+        return;
+	
+	int m = (l+r)/2, median = l;
+	
+	if(((v[l] < v[m]) && (v[m] < v[r])) || ((v[r] < v[m]) && (v[m] < v[l])))
+		median = m;
+	else if(((v[m] < v[l]) && (v[l] < v[r])) || ((v[r] < v[l]) && (v[l] < v[m])))
+		median = l;
+	else 
+		median = r;
+	
+	int t = v[median];
+	v[median] = v[l];
+	v[l] = t;
+	
+    c += (r-l);
+	
+    int q = partition(v, l, r);
+    quickSort(v, l, q-1);
+    quickSort(v, q+1, r);
+}
+
+int main() {
+    vector<int> v;
+    int n;
+    for(int i=0; i<10000; ++i) {
+        cin >> n;
+        v.push_back(n);
+    }
+
+    quickSort(v, 0, 9999);
+
+//    for(int i=0; i<10000; ++i) {
+//        cout << v[i] << " ";
+//    }
+
+    cout << c ;
+
+}
