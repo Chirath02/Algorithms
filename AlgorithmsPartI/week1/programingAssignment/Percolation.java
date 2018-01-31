a0d026c177cb4cba24ae69b7d28b6a3cead66b01
Chirath R <chirath.02@gmail.com>
2018-01-31 09:08:07 +0530

    completed assignment 1

--- AlgorithmsPartI/week1/programingAssignment/Percolation.java
+++ AlgorithmsPartI/week1/programingAssignment/Percolation.java
@@ -2,72 +2,93 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-    private final int numberOfSites;
+    private final int numberOfSites, virtualTop, virtualBottom;
     private int numberOfOpenSites;
     private boolean[][] openSites;
-    private final WeightedQuickUnionUF uf;
+    private final WeightedQuickUnionUF uf, ufVirtualTopOnly;
 
-    public Percolation(int n) {               // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        // create n-by-n grid, with all sites blocked
         if (n <= 0)
             throw new java.lang.IllegalArgumentException();
         uf = new WeightedQuickUnionUF(n * n + 2);
+        ufVirtualTopOnly = new WeightedQuickUnionUF(n * n + 2);
         numberOfSites = n;
         numberOfOpenSites = 0;
+        virtualTop = 0;
+        virtualBottom = (n * n) + 1;
         openSites = new boolean[n][n];
-        for (int i = 0; i < n; ++i)
-            uf.union(0, i + 1);
-    }
 
-    public void open(int row, int col) {   // open site (row, col) if it is not open already
-        isInBound(row, col);
-        if (isOpen(row, col))
-            return;
-        numberOfOpenSites++;
-        openSites[row - 1][col - 1] = true;
-        int siteNumber = numberOfSites * (row - 1) + col;
-        // left
-        if (col > 1 && isOpen(row, col - 1))
-            uf.union(siteNumber, numberOfSites * (row - 1) + col - 1);
-        // right
-        if (col < numberOfSites && isOpen(row, col + 1))
-            uf.union(siteNumber, numberOfSites * (row - 1) + col + 1);
-        // up
-        if (row > 1 && isOpen(row - 1, col))
-            uf.union(siteNumber, numberOfSites * (row - 2) + col);
-        // down
-        if (row < numberOfSites && isOpen(row + 1, col))
-            uf.union(siteNumber, numberOfSites * row + col);
-
-        // connect to virtual site at bottom if the site is in the last row
-        for (int i = 1; i <= numberOfSites; ++i) {
-            siteNumber = ((numberOfSites - 1) * numberOfSites) + i;
-            if (uf.connected(0, siteNumber))
-                uf.union(siteNumber, (numberOfSites * numberOfSites) + 1);
+        for (int i = 1; i <= n; ++i) {
+            uf.union(virtualTop, i);
+            ufVirtualTopOnly.union(virtualTop, i);
         }
     }
 
-    public boolean isOpen(int row, int col) { // is site (row, col) open?
+    private int convertToIndex(int row, int col) {
+        return (numberOfSites * (row - 1)) + col;
+    }
+
+    public void open(int row, int col) {
+        // open site (row, col) if it is not open already
         isInBound(row, col);
-        // StdOut.println(row + " "  + col + openSites[row - 1][col - 1]);
+        if (isOpen(row, col))
+            return;
+
+        numberOfOpenSites++;
+        openSites[row - 1][col - 1] = true;
+
+        int siteNumber = convertToIndex(row, col);
+        // left
+        if (col > 1 && isOpen(row, col - 1)) {
+            uf.union(siteNumber, convertToIndex(row, col - 1));
+            ufVirtualTopOnly.union(siteNumber, convertToIndex(row, col - 1));
+        }
+        // right
+        if (col < numberOfSites && isOpen(row, col + 1)) {
+            uf.union(siteNumber, convertToIndex(row, col + 1));
+            ufVirtualTopOnly.union(siteNumber, convertToIndex(row, col + 1));
+        }
+        // up
+        if (row > 1 && isOpen(row - 1, col)) {
+            uf.union(siteNumber, convertToIndex(row - 1, col));
+            ufVirtualTopOnly.union(siteNumber, convertToIndex(row - 1, col));
+        }
+        // down
+        if (row < numberOfSites && isOpen(row + 1, col)) {
+            uf.union(siteNumber, convertToIndex(row + 1, col));
+            ufVirtualTopOnly.union(siteNumber, convertToIndex(row + 1, col));
+        }
+
+        if (row == numberOfSites) {
+            uf.union(virtualBottom, siteNumber);
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        // is site (row, col) open?
+        isInBound(row, col);
         return openSites[row - 1][col - 1];
     }
 
-    public boolean isFull(int row, int col) { // is site (row, col) full?
+    public boolean isFull(int row, int col) {
+        // is site (row, col) full?
         isInBound(row, col);
-        // StdOut.println(uf.connected(0, numberOfSites * (row - 1) + col));
-        return isOpen(row, col) && uf.connected(0, numberOfSites * (row - 1) + col);
+        return isOpen(row, col) && ufVirtualTopOnly.connected(0, convertToIndex(row, col));
     }
 
-    public int numberOfOpenSites() {     // number of open sites
+    public int numberOfOpenSites() {
+        // number of open sites
         return numberOfOpenSites;
     }
 
-    public boolean percolates() {            // does the system percolate?
-        return uf.connected(0, numberOfSites * numberOfSites + 1);
+    public boolean percolates() {
+        // does the system percolate?
+        return uf.connected(virtualTop, virtualBottom);
     }
 
-    public static void main(String[] args) {  // test client (optional)
-
+    public static void main(String[] args) {
+        // test client (optional)
     }
 
     private void isInBound(int row, int col) {
--- AlgorithmsPartI/week1/programingAssignment/PercolationStats.java
+++ AlgorithmsPartI/week1/programingAssignment/PercolationStats.java
@@ -5,10 +5,12 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
     private final int n, trials;
-    private final double meanResult, stddevResult, confidence95 = 1.96;
+    private final double meanResult, stddevResult;
+    private final static double confidence95 = 1.96;
     private double[] results;
 
-    public PercolationStats(int n, int trials) {    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        // perform trials independent experiments on an n-by-n grid
         if (n <= 0  || trials <= 0)
             throw new java.lang.IllegalArgumentException();
         this.n = n;
@@ -35,19 +37,24 @@ public class PercolationStats {
              results[i] = trial();
     }
 
-    public double mean() {                          // sample mean of percolation threshold
+    public double mean() {
+        // sample mean of percolation threshold
         return meanResult;
     }
-    public double stddev() {                        // sample standard deviation of percolation threshold
+    public double stddev() {
+        // sample standard deviation of percolation threshold
         return stddevResult;
     }
-    public double confidenceLo() {                  // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        // low  endpoint of 95% confidence interval
         return (meanResult - ((confidence95 * stddevResult) / Math.sqrt(trials)));
     }
-    public double confidenceHi() {                  // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        // high endpoint of 95% confidence interval
         return (meanResult + ((confidence95 * stddevResult) / Math.sqrt(trials)));
     }
-    public static void main(String[] args) {       // test client (described below)
+    public static void main(String[] args) {
+        // test client (described below)
         PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         StdOut.println("mean\t\t\t= " + percolationStats.mean());
         StdOut.println("stddev\t\t\t= " + percolationStats.stddev());
